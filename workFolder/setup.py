c2a35c4af1d3d8d10dd97db69bfd1ccc4b1894aa
MaximeLanca <lanca.maxime.ber@gmail.com>
2023-08-02 08:55:51 +0200

    Avancement du fichier setup.py

--- workFolder/setup.py
+++ workFolder/setup.py
@@ -4,10 +4,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 
-response= requests.get(url)
-print(response)
+response = requests.get(url)
 
+if response.ok:
+    # Vérification de la connexion
+    soup = BeautifulSoup(response.text)
+
+    #Recupere le titre du livre
+    title = soup.find('title')
+    print(title.text)
+
+    #Récuperation des données de la page HTML
+    data = {}
+    tdAttribute = soup.findAll('table')
+    for trs in tdAttribute:
+        tr = trs.findAll('tr')
+        for ths in tr:
+            th = ths.find('th')
+            td = ths.find('td')
+            ##print(str(th) + ' : ' + str(td) + '\n')
 
 
