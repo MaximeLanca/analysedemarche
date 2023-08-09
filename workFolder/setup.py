63e6d488632cfb7a0a06fdd3333d558b5eea64fe
MaximeLanca <lanca.maxime.ber@gmail.com>
2023-08-09 17:41:57 +0200

    maj

--- workFolder/setup.py
+++ workFolder/setup.py
@@ -12,19 +12,29 @@ if response.ok:
     # Connection check
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Title extraction
-    title = soup.find('title')
-    print('Titre de la page HTML :', title.text)
+    #Data extraction part one from HTML
+    dataPartOne = []
+    classAttribut = soup.find('div', {'class': 'col-sm-6 product_main'})
+    for classAttributs in classAttribut :
+        dataPartOne.append(classAttributs)
+
+    titleBook = (str(dataPartOne[1])).replace('<h1>', '').replace('</h1>', '')
+
+    availability = str(dataPartOne[5]).splitlines()
+    availableBook= availability[3]
+    print(availableBook)
+
 
     # Product description extraction
     description = soup.find('div', {'id': 'content_inner'}).findAll('p')
     descriptionList = []
     for a in description:
         descriptionList.append(a)
-    print('Description du livre :' + '\n', descriptionList[3].text)
+    productDescription = descriptionList[3].text.replace(',', '')
+    print(productDescription)
 
-    # Data extraction from HTML
-    data = {}
+    # Data extraction part two from HTML
+    dataPartTwo = {}
     tdAttribute = soup.findAll('table')
     for trs in tdAttribute:
         tr = trs.findAll('tr')
@@ -32,16 +42,16 @@ if response.ok:
         for ths in tr:
             th = ths.find('th')
             td = ths.find('td')
-            data[th.text] = td.text
-    print('Données de la page HTML :' + '\n', data)
+            dataPartTwo[th.text] = td.text
+    #print('Données de la page HTML :' + '\n', data)
 
     # Picture extraction
     picture = soup.find('img')
-    print('Lien de la converture du livre :' + '\n', picture.get('src'))
+    # print('Lien de la converture du livre :' + '\n', picture.get('src'))
 
     # review_rating extraction
     reviewRating = soup.find_all('p')
-    listRating=str(reviewRating[2])
+    listRating = str(reviewRating[2])
     splitlinesList = listRating.splitlines()
     replaceElementList = str(splitlinesList[0]).replace('<p class="', '').replace('">', '').lower()
     listTransformation= replaceElementList.split()
@@ -54,11 +64,11 @@ if response.ok:
             case 'two':
                 starNumbers= 2
             case 'three':
-                starNumbers=3
+                starNumbers= 3
             case 'four':
-                starNumbers=4
+                starNumbers= 4
             case 'five':
-                starNumbers=5
+                starNumbers= 5
 
     # category extraction
 
@@ -66,7 +76,9 @@ if response.ok:
     # csv.file creation
     with open('Information_du_livre.csv', 'w') as outf:
         outf.write('product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url\n')
-        outf.write(str(url) + ',' + data['UPC'] + ',' + str(title.text) + ',' + data['Price (excl. tax)'] + ',' + data['Price (incl. tax)'] + ',' + data['Availability'] + ',' + str(descriptionList[3].text) + ',' + str(starNumbers) + ',' + picture.get('src'))
+        outf.write(str(url) + ',' + dataPartTwo['UPC'] + ' , ' + titleBook + ',' + dataPartTwo['Price (excl. tax)'] + ',' + dataPartTwo['Price (incl. tax)'] + ',' + availableBook + ',' + productDescription)
+
+        ##outf.write(str(url) + ',' + data['UPC'] + ',' + str(title.text) + ',' + data['Price (excl. tax)'] + ',' + data['Price (incl. tax)'] + ',' + data['Availability'] + ',' + str(descriptionList[3].text) + ',' + str(starNumbers) + ',' + picture.get('src'))
 
     
 ##product_page_url
