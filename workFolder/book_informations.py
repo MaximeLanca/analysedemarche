cda41d6da54cb8e435c07609410567b0d95c2ea8
MaximeLanca <lanca.maxime.ber@gmail.com>
2023-08-19 01:46:27 +0200

    Mise à jour des variables

--- workFolder/book_informations.py
+++ workFolder/book_informations.py
@@ -11,12 +11,12 @@ from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 
-def book_informations():
+def book_informations_from_page():
     """
     The fonction returns main informations of book
     """
-    url_book_in_web_site = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-    response = requests.get(url_book_in_web_site)
+    url_book = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+    response = requests.get(url_book)
     # Connection check
     if response.ok:
 
@@ -26,7 +26,8 @@ def book_informations():
         book_title_in_page_web = soup.find('h1').text
 
         # Availability book
-        book_availability_in_page_web = soup.find('p', class_='instock availability').text
+        book_availability_in_page_web = (soup.find('p', class_='instock availability').text).strip
+
 
         # Review rating book
         dictionary_for_search_rating = {'One': '1/5', 'Two': '2/5', 'Three': '3/5', 'Four': '4/5', '<Five': '5/5'}
@@ -51,7 +52,7 @@ def book_informations():
         picture_in_page_web = soup.find('img')
         web_link_book = picture_in_page_web.get('src')
         if web_link_book:
-            web_link_book = urljoin(url_book_in_web_site, web_link_book)
+            web_link_book = urljoin(url_book, web_link_book)
 
         picture_url_book = requests.get(web_link_book).content
         image_file_book_in_page_web = io.BytesIO(picture_url_book)
@@ -63,31 +64,25 @@ def book_informations():
         description_book_in_page_web = (soup.find('p', class_=False)).text
 
         # category extraction
-        #category_in_page_web= soup.find('a', href=True)
-        
-        category_in_page_web = soup.find_all('a')
-        list_for_extract_book_information = []
-        for a in category_in_page_web:
-            list_for_extract_book_information.append(a.text)
-        print(list_for_extract_book_information)
+        category_in_page_web = soup.find('a', href="../category/books/poetry_23/index.html")
+        print(category_in_page_web.text)
 
         # csv.file creation
         with open('../output/data_book.csv', 'w', newline='', encoding='utf-8') as csvfile:
             fieldnames = ['URL', 'UPC', 'Book Title', 'Price (excl. tax)', 'Price (incl. tax)', 'Number_available',
                           'Product description', 'Category book', 'Review_rating', 'Picture URL']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
             writer.writeheader()
-            writer.writerow({'URL': url_book_in_web_site,
+            writer.writerow({'URL': url_book,
                              'UPC': dictionary_for_put_book_information_from_page_web['UPC'],
                              'Book Title': book_title_in_page_web,
-                             'Price (excl. tax)': dictionary_for_put_book_information_from_page_web[
-                                 'Price (excl. tax)'],
-                             'Price (incl. tax)': dictionary_for_put_book_information_from_page_web[
-                                 'Price (incl. tax)'],
+                             'Price (excl. tax)': dictionary_for_put_book_information_from_page_web
+                             ['Price (excl. tax)'],
+                             'Price (incl. tax)': dictionary_for_put_book_information_from_page_web
+                             ['Price (incl. tax)'],
                              'Number_available': book_availability_in_page_web,
                              'Product description': description_book_in_page_web,
-                             'Category book': list_for_extract_book_information,
+                             'Category book': category_in_page_web.text,
                              'Review_rating': rating_book_extract_of_dictionary,
                              'Picture URL': web_link_book
                              })
@@ -96,4 +91,4 @@ def book_informations():
         print('-- Connexion server error --')
 
 
-book_informations()
+book_informations_from_page()
