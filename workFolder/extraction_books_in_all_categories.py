946def855368ff16bcc4d1750761c36893e39c0a
MaximeLanca <lanca.maxime.ber@gmail.com>
2023-09-01 23:44:41 +0200

    Fichier extraction_books_in_all_categories finalisé

--- workFolder/extraction_books_in_all_categories.py
+++ workFolder/extraction_books_in_all_categories.py
@@ -12,9 +12,10 @@ def select_all_url_categories() -> list:
     for category_link in soup.find('ul', class_='nav nav-list').find('ul').find_all('a'):
         categories_url_reconstitution.append(books_toscrape_url + category_link['href'])
     books_url_reconstitution = []
-    for a in categories_url_reconstitution:
-        soup = connection_check_url(a)
-        b = soup.find('ol', class_='row').find('h3').find('a')
-        books_url_reconstitution.append((link_book_from_catalogue_web_page + b['href']).replace('../../../', ''))
-    return b
+    for links_from_categories_url_reconstitution in categories_url_reconstitution:
+        soup = connection_check_url(links_from_categories_url_reconstitution)
+        books_links_from_web_page = soup.find('ol', class_='row').find('h3').find('a')
+        books_url_reconstitution.append((link_book_from_catalogue_web_page + books_links_from_web_page['href'])
+                                        .replace('../../../', ''))
+    return books_url_reconstitution
 
